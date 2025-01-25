e07863a76c3dd83df62680bde90a2b00add19313
wanzofc <berlianawan498@gmail.com>
2025-01-25 20:34:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'wanzofc-tech'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "berlianawan148@gmail.com" //email
+global.email_password = "olre djtq lzyu oaxg" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
